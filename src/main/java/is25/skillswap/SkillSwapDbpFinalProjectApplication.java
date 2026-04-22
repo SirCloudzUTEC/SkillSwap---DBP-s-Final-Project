2bf3236f4af701677cc7d3d2ffd0bc32cca1ac38
Diego Godoy <diego.godoy.t@utec.edu.pe>
2026-04-22 07:42:38 -0400

    Base

---
+++ src/main/java/is25/skillswap/SkillSwapDbpFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package is25.skillswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSwapDbpFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSwapDbpFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is25/skillswap/SkillSwapDbpFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package is25.skillswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapDbpFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
